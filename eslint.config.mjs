f925172347135c458450b519c29c66b3c913f256
Siradio7 <mamadousiradioudiallo4@gmail.com>
2026-05-07 03:00:52 +0200

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   {
@@ -19,4 +20,5 @@ export default defineConfig([
       sourceType: "commonjs"
     }
   },
+  prettier
 ]);
